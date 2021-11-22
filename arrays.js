5737ce7ff619eb6fe8fe57710901139ffa4430d9
Sisi Hu <shu004@gmail.com>
2021-11-22 16:50:39 -0700

    arrays.js complete

--- arrays.js
+++ arrays.js
@@ -2,15 +2,31 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+  for (const i in items) {
+    console.log(`${items[i]} ${[i]}`)
+  }
 }
+
+// printIndices(['apple', 'berry', 'cherry'])
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  const result = []
+  for (const i in items) {
+    if (i%2 === 0) {
+      result.push(items[i])
+    }
+  }
+  console.log(result)
 }
+
+// everyOtherItem(['apple', 'berry', 'cherry'])
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  const sortedItems = items.sort((a, b) => a - b)
+  const sortedNItems = sortedItems.slice(0, n)
+  console.log(sortedNItems)
 }
+//ask them about this problem re: python reverse()
+// smallestNItems([1, 12, 4, 2], 3)
