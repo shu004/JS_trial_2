1688dd3f7418709c365b18562abf47b261c8fb9c
Sisi Hu <shu004@gmail.com>
2021-11-22 17:11:20 -0700

    functions js complete

--- functions.js
+++ functions.js
@@ -3,11 +3,45 @@
 // 1. isHometown
 
 // Define your function here
+// const is_hometown = (town) =>{
+//     if (town === "San Francisco") {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+const isHometown = town => town === "San Francisco"
+
+// console.log(is_hometown("San Francisco"))
 
 // 2. getFullName
 
-// Define your function here
+// const getFullName = (firstName, lastName) => {
+//     return `${firstName} ${lastName}`;
+// }
+
+const getFullName = (firstName, lastName) => `${firstName} ${lastName}`
+
+console.log(getFullName("Renee", "Lee"))
 
 // 3. calculateTotal
+const calculateTotal = (basePrice, state, tax=0.05) => {
+    const subtotal = basePrice * (1 + tax);
+    let fee = 0;
+    if (state === "CA") {
+        fee = 0.03 * subtotal
+    } else if (state === "PA") {
+        fee = 2
+    } else if (state === "MA") {
+        if (basePrice <= 100) {
+            fee = 1
+        } else {
+            fee = 3
+        }
+    }
+    return subtotal + fee
+}
 
-// Define your function here
+// console.log(calculateTotal(3, "PA"))
+
